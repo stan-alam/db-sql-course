07f91d6d2f141805726ee8e6c019ee2b7f24fd6d
stanislaw alam <stan.alam@gmail.com>
2024-07-06 20:03:32 -0400

    added db T-SQl course work

---
+++ T-SQL/images/01/17.sql
@@ -0,0 +1,39 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [EmployeeID]
+      ,[LastName]
+      ,[FirstName]
+      ,[Title]
+      ,[TitleOfCourtesy]
+      ,[BirthDate]
+      ,[HireDate]
+      ,[Address]
+      ,[City]
+      ,[Region]
+      ,[PostalCode]
+      ,[Country]
+      ,[HomePhone]
+      ,[Extension]
+      ,[Notes]
+      ,[ReportsTo]
+      ,[PhotoPath]
+  FROM [AdventureWorks].[dbo].[Employees]
+
+  select FirstName, LastName, Title, BirthDate from AdventureWorks.dbo.Employees Order by BirthDate
+
+  select FirstName, LastName, Title, BirthDate = convert(date, BirthDate) from AdventureWorks.dbo.Employees Order By BirthDate
+  --12
+  select FirstName, LastName, FullName = FirstName + ' ' + LastName from AdventureWorks.dbo.Employees
+  --13
+  select TotalPrice = UnitPrice * Quantity, OrderID, ProductID, UnitPrice, Quantity from AdventureWorks.dbo.OrderDetails Order By
+  OrderID, ProductID
+  --14
+  select TotalCustomer = count(*) from AdventureWorks.dbo.Customers
+  --15
+  select FirstOrder = min(OrderDate) from AdventureWorks.dbo.Orders
+  --------
+  --16
+  select Country from AdventureWorks.dbo.Customers Group By Country
+  --17
+  --contact titles for customers
+  -- discuss 
+  select ContactTitle, TotalContactTitle = count(*) from AdventureWorks.dbo.Customers Group By ContactTitle Order By count(*) desc
